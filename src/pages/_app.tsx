ee55af2a2a4cd9df5e3599f637fd2065346d1c06
dhunganabibek <Dhunganabibek2016@gmail.com>
2023-02-16 12:09:22 -0600

    Layout component added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,8 @@ import Layout from "@/components/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
+    <>
       <Component {...pageProps} />
-    </Layout>
+    </>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,17 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
 import Header from "../components/Header";
-import Footer from "../components/Footer"
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>Open Market Hub</title>
-        <meta name="OpenMarketHub" content="MarketHub for selling goods and products" />
+        <meta
+          name="OpenMarketHub"
+          content="MarketHub for selling goods and products"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
